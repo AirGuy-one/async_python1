963dc7d817d09fcaf434843fd547c02565242bba
AirGuy-one <airguy-one@yandex.ru>
2022-12-03 13:55:42 +0300

    Now we have sky with twinkling stars and we do it using an asynchronous function

--- main.py
+++ main.py
@@ -7,45 +7,46 @@ async def blink(canvas):
     window_tmp = curses.initscr()
 
     x_max, y_max = window_tmp.getmaxyx()
-    data_x, data_y = [], []
-    for i in range(10):
-        data_x.append(random.randint(1, x_max - 1))
-        data_y.append(random.randint(1, y_max - 1))
 
-    for i in range(10):
-        canvas.border()
-        curses.curs_set(False)
-        row, column = data_x[i], data_y[i]
-        symbol = "*"
-        data_sec = [2, 0.3, 0.5, 0.3]
+    canvas.border()
+    curses.curs_set(False)
+    symbol = "*"
 
-        for j in range(4):
-            if j % 2 != 0:
-                canvas.addstr(row, column, symbol)
-                canvas.refresh()
-                await asyncio.sleep(data_sec[j])
-            elif j == 0:
-                canvas.addstr(row, column, symbol, curses.A_DIM)
-                canvas.refresh()
-                await asyncio.sleep(data_sec[j])
-            else:
-                canvas.addstr(row, column, symbol, curses.A_BOLD)
-                canvas.refresh()
-                await asyncio.sleep(data_sec[j])
+    async def tmp():
+        while True:
+            row = random.randint(1, x_max - 1)
+            column = random.randint(1, y_max - 1)
+
+            canvas.addstr(row, column, symbol, curses.A_DIM)
+            canvas.refresh()
+            await asyncio.sleep(2)
+
+            canvas.addstr(row, column, symbol)
+            canvas.refresh()
+            await asyncio.sleep(0.3)
+
+            canvas.addstr(row, column, symbol, curses.A_BOLD)
+            canvas.refresh()
+            await asyncio.sleep(0.5)
+
+            canvas.addstr(row, column, symbol)
+            canvas.refresh()
+            await asyncio.sleep(0.3)
+
+            
+
+    asyncio.create_task(tmp())
+    asyncio.create_task(tmp())
+    asyncio.create_task(tmp())
+    asyncio.create_task(tmp())
+    asyncio.create_task(tmp())
+    
+
 
 
 if __name__ == '__main__':
     curses.update_lines_cols()
-    asyncio.run(curses.wrapper(blink))
 
-# async def count_to_three():
-#     print("Веду отсчёт. 1")
-#     await asyncio.sleep(0)
-#     print("Веду отсчёт. 2")
-#     await asyncio.sleep(0)
-#     print("Веду отсчёт. 3")
-#     await asyncio.sleep(0)
-#
-# if __name__ == '__main__':
-#     asyncio.run(count_to_three())
-
+    main_loop = asyncio.get_event_loop()
+    main_loop.run_until_complete(curses.wrapper(blink))
+    main_loop.run_forever()
