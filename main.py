abc50d17e3f6a868c5cf3d1e0fc5b70a3baaee73
AirGuy-one <airguy-one@yandex.ru>
2022-12-03 16:38:25 +0300

    Add fire animation

--- main.py
+++ main.py
@@ -3,6 +3,36 @@ import curses
 import asyncio
 
 
+async def fire(canvas, start_row, start_column, rows_speed=-1, columns_speed=0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(1)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(1)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 0 < row < max_row and 0 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def blink(canvas):
 
     list_of_symbols = ["*", "+", "•"]
@@ -15,8 +45,8 @@ async def blink(canvas):
     curses.curs_set(False)
 
     async def tmp():
-        row = random.randint(1, x_max - 1)
-        column = random.randint(1, y_max - 1)
+        row = random.randint(2, x_max - 2)
+        column = random.randint(2, y_max - 2)
         symbol = random.choice(list_of_symbols)
 
         while True:
@@ -35,6 +65,9 @@ async def blink(canvas):
             canvas.addstr(row, column, symbol)
             canvas.refresh()
             await asyncio.sleep(0.3)
+
+    asyncio.create_task(fire(canvas, 7, 20))
+
 
     for i in range(50):
         wait_time = random.uniform(0.2, 0.5)
